f2729c79d9d5194fb4d9903a77e87c5a469f0289
javierehz <javiehz@hotmail.com>
2026-06-17 12:23:32 +0200

    Category and Product Repository implemented

---
+++ src/main/java/com/javieh/javitech/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.javieh.javitech.repository;
+
+import com.javieh.javitech.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findByUserId(Long userId);
+}
