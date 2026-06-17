67d719d823079c7895fe288b29dfa7b7639977a1
javierehz <javiehz@hotmail.com>
2026-06-17 11:10:14 +0200

    Cart Entity

---
+++ src/main/java/com/javieh/javitech/entity/Cart.java
@@ -0,0 +1,21 @@
+package com.javieh.javitech.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+}
